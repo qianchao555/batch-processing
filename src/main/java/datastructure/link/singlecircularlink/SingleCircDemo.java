db00404e6d828d5a204df728a1aba08af33cb10d
qianchao <qianchao@travelsky.com.cn>
2021-11-25 17:27:29 +0800

    2021-11-25:约瑟夫 自我实现

--- src/main/java/datastructure/link/singlecircularlink/SingleCircDemo.java
+++ src/main/java/datastructure/link/singlecircularlink/SingleCircDemo.java
@@ -10,8 +10,11 @@ public class SingleCircDemo {
     public static void main(String[] args) {
         CircleSingleLink circleSingleLink=new CircleSingleLink();
         //加入5个节点
-        circleSingleLink.addBoy(5);
-        circleSingleLink.showCircleLink();
-        circleSingleLink.countBoy(1,2,5);
+//        circleSingleLink.addBoy(5);
+//        circleSingleLink.showCircleLink();
+//        circleSingleLink.countBoy(1,2,5);
+        MyJosephuCicle myJosephuCicle=new MyJosephuCicle();
+        myJosephuCicle.addNode(5);
+        myJosephuCicle.countNode(1,2,5);
     }
 }
