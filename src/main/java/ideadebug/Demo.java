4c223032f00f31dd71c12f853024f1bba2bfccf2
qianchao <qianchao@travelsky.com.cn>
2021-12-17 17:32:30 +0800

    2021-12-17 Jvm

---
+++ src/main/java/ideadebug/Demo.java
@@ -0,0 +1,18 @@
+package ideadebug;
+
+/**
+ * Evaluate debug调试工具
+ * 在debug调试代码时，如何更快的定位问题，并运行指定代码
+ *
+ * @Author qianchao
+ * @Date 2021/12/10
+ * @Version OPRA V1.0
+ **/
+public class Demo {
+    public static void main(String[] args) {
+        for (int  i = 0;  i < 10;  i++) {
+            System.out.println("這次的值是："+i);
+        }
+    }
+
+}
