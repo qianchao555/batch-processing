ef3ef3467a2cc9f2a35cb2e739fc7206466b45cd
qc <xiaoyigedl@gmail.com>
2022-05-04 23:10:42 +0800

    kafka-2

--- src/main/java/juc/Test.java
+++ src/main/java/juc/Test.java
@@ -1,9 +1,7 @@
 package juc;
 
-import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 /**
  * @description:
@@ -13,17 +11,12 @@ import java.util.concurrent.TimeUnit;
  */
 public class Test {
     public static void main(String[] args) {
-        ConcurrentHashMap concurrentHashMap=new ConcurrentHashMap();
-        concurrentHashMap.put("name","qc");
-        concurrentHashMap.get("name");
-        ThreadPoolExecutor threadPoolExecutor=new ThreadPoolExecutor(1,2,1,TimeUnit.SECONDS,new ArrayBlockingQueue(10));
-//        threadPoolExecutor.execute();
-        Long a=10L;
-        a=null;
-        Integer b=null;
-         System.out.println(b==null?null:Long.valueOf(5));
-//        threadPoolExecutor.execute();
-        String s;
-        Object object;
+        DateTimeFormatter df =DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        LocalDateTime localDateTime=LocalDateTime.parse("2022-05-03 14:09:34",df);
+//        LocalDateTime localDateTime=LocalDateTime.now();
+
+        System.out.println(localDateTime);
+
+        System.out.println(localDateTime.plusNanos(754927277000L));
     }
 }
