3b894c97e4774722235c4f1d4a45724f8e4f82ef
qc <xiaoyigedl@gmail.com>
2022-06-26 22:49:38 +0800

    2022-06-26 1、logback+ELK搭建日志 2、springboot整合elasticsearch并通过ElasticsearchRestTemplate实现查询

---
+++ src/main/java/com/springbootproject/controller/Log4jLogstashController.java
@@ -0,0 +1,36 @@
+package com.springbootproject.controller;
+
+import com.springbootproject.service.Log4jLogstashService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @description:
+ * @author:xiaoyige
+ * @createTime:2022/6/25 17:35
+ * @version:1.0
+ */
+
+@Slf4j
+@RestController
+public class Log4jLogstashController {
+    @Autowired
+    Log4jLogstashService log4jLogstashService;
+
+    @GetMapping("/log4jLogstashTest")
+    public String showLog() {
+        log.warn("555warn日志测试3,啦啦啦啦,lalala");
+        log.info("555info日志测试3,啦啦啦啦,lalala");
+        log.debug("555debug日志测试3,啦啦啦啦,lalala");
+        log4jLogstashService.logPrint();
+        System.out.println("sysout能打印出来吗？");
+        return "ok";
+
+    }
+    @GetMapping("/testES")
+    public void testEs(){
+        log4jLogstashService.testEs();
+    }
+}
